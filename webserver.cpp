0e30fb006f7a25511522c4f7389c97ff956559b7
humzafazal72 <abbasisaud99@gmail.com>
2025-05-11 20:48:08 +0500

    created a simple webserver with basic routing

---
+++ webserver.cpp
@@ -0,0 +1,135 @@
+#include <arpa/inet.h>
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+
+using namespace std;
+
+static int PORT = 8080; 
+
+/*                              Utilities                                     */
+struct HttpResponse;
+unordered_map<string, HttpResponse(*)()> routes;
+
+vector<string> split_string(const string& str) {
+    vector<string> words;
+    stringstream ss(str);
+    string word;
+    while (ss >> word) {
+        words.push_back(word);
+    }
+    return words;
+}
+
+class HttpRequest {
+public:
+    string method;
+    string path;
+    string body;
+    map<string, string> headers;
+
+    HttpRequest(string m, string p) {
+        method = m;
+        path = p;
+    }
+};
+
+struct HttpResponse {
+    string response_code;
+    string status_text;
+    map<string, string> headers;
+    string body;
+};
+
+/*                              Routes                                        */
+HttpResponse index() {
+    HttpResponse response;
+    response.response_code = "200";
+    response.status_text = "OK";
+    response.headers["Content-Type"] = "text/html";
+    response.body = "<html><body><h1> Index Page </h1></body></html>";
+    return response;
+}
+
+/*                              Driver Function                               */
+int main() {
+    // Register routes
+    routes["GET /"] = &index;
+
+    // init Socket 
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0) {
+        cerr << "Failed to create socket" << endl;
+        return 1;
+    }
+
+    int opt = 1;
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        cerr << "setsockopt failed" << endl;
+        return 1;
+    }
+
+    // Bind 
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    address.sin_port = htons(PORT);
+
+    if (bind(socket_fd, (struct sockaddr*)&address, sizeof(address))) {
+        cerr << "Failed to bind" << endl;
+        return 1;
+    }
+
+    // Listen
+    if (listen(socket_fd, 1)) {
+        cerr << "Failed to listen" << endl;
+        return 1;
+    }
+    cout << "Listening on 127.0.0.1:8080..." << endl;
+
+    while (true) {
+        int client_fd = accept(socket_fd, nullptr, nullptr);
+        char buffer[1024] = {0};
+        int bytes_read = read(client_fd, buffer, sizeof(buffer));
+
+        // Parsing and Response
+        if (bytes_read > 0) {
+            buffer[bytes_read] = '\0';
+            istringstream stream(buffer);
+            string line;
+            getline(stream, line);
+
+            if (line.find("/favicon.ico") == string::npos && line.find("GET") != string::npos) {
+                vector<string> header = split_string(line);
+                string route_key = header[0] + " " + header[1];
+
+                if (routes.find(route_key) != routes.end()) {
+                    HttpResponse response = routes[route_key]();
+                    string res = 
+                        "HTTP/1.1 " + response.response_code + " " + response.status_text + "\r\n" +
+                        "Content-Type: " + response.headers["Content-Type"] + "\r\n\n" +
+                        response.body;
+
+                    send(client_fd, res.c_str(), res.size(), 0);
+                } else {
+                    string not_found = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\n404 Not Found";
+                    send(client_fd, not_found.c_str(), not_found.size(), 0);
+                }
+            } else {
+                cout << "Unsupported method." << endl;
+            }
+            close(client_fd);
+        } else {
+            cerr << "Failed to read data" << endl;
+            close(client_fd);
+        }
+    }
+    return 0;
+}
