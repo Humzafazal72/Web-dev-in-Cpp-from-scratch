51236f28c73b7fe4505435a21160a4e9547df55c
humzafazal72 <abbasisaud99@gmail.com>
2025-05-19 22:44:04 +0500

    added delete_item api + improved redirection

--- webserver.cpp
+++ webserver.cpp
@@ -18,8 +18,7 @@ static int PORT = 8080;
 
 /*                              Utilities                                     */
 struct HttpResponse;
-unordered_map<string, HttpResponse(*)()> get_routes;
-unordered_map<string, HttpResponse(*)(string)> post_routes;
+unordered_map<string, HttpResponse(*)(string)> routes;
 
 vector<string> split_string(const string& str) {
     vector<string> words;
@@ -52,7 +51,18 @@ struct HttpResponse {
 };
 
 /*                              Handlers                                        */
-HttpResponse index() {
+HttpResponse redirect_to_index() {
+    HttpResponse response;
+    response.response_code = "303";
+    response.status_text = "See Other";
+    response.headers["Location"] = "/";
+    response.headers["Content-Type"] = "text/plain"; // Optional
+    response.body = "Redirecting...";
+    return response;
+}
+
+
+HttpResponse index(string arg) {
     ifstream html("index.html");
     ifstream file("items.txt");
     if (!html.is_open()){
@@ -69,9 +79,11 @@ HttpResponse index() {
 
     string lines="";string line;
     while(getline(file,line)){
-        lines+="<li>"+line+"</li>\n";
+        lines += "<li>" + line + 
+         " <a href=\"delete_item?item=" + line + 
+         "\" style=\"margin-left:10px;padding:4px 8px;background-color:#ff4d4d;color:white;border:none;border-radius:4px;text-decoration:none;font-size:0.9em;\">Delete</a></li>\n";
+
     }    
-    cout<<buffer.str()<<endl;
     HttpResponse response;
     response.response_code = "200";
     response.status_text = "OK";
@@ -83,17 +95,36 @@ HttpResponse index() {
 HttpResponse add_item(string item){
     ofstream file("items.txt", ios::app);
     file << item << "\n";
-    file.close();
-    
-    return index();
+    file.close();    
+    return redirect_to_index();
 }
 
+HttpResponse delete_item(string item){
+    item = regex_replace(item,regex("%20")," ");
+    ifstream infile("items.txt");
+    string line,items="";
+    while(getline(infile,line)){
+        items.append(line);
+        items.append("&");
+    }
+    items = regex_replace(items,regex(item+"&"),"");
+    items = regex_replace(items,regex("&"),"\n");
+
+    infile.close();
+
+    ofstream outfile("items.txt",ios::out);
+    outfile << items;
+
+    return redirect_to_index();
+}
 
 /*                              Driver Function                               */
 int main() {
     // Register routes
-    get_routes["GET /"] = &index;
-    post_routes["POST /add_item"] = &add_item;
+    routes["GET /"] = &index;
+    routes["GET /delete_item"]=&delete_item;
+    routes["POST /add_item"] = &add_item;
+
 
     // init Socket 
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -140,14 +171,24 @@ int main() {
 
             if (line.find("/favicon.ico") == string::npos && line.find("GET") != string::npos) {
                 vector<string> header = split_string(line);
-                string route_key = header[0] + " " + header[1];
+                string arg=" ";
+                string route_key;
+                if (header[1].find("?")==string::npos){
+                    route_key = header[0] + " " + header[1];
+                } else{
+                    int pos = header[1].find("?"); 
+                    route_key = header[0] + " " + header[1].substr(0,pos);
+                    arg = header[1].substr(pos+6,header[1].length());
+                    cout<< route_key << " " << arg << endl;
+                }
 
-                if (get_routes.find(route_key) != get_routes.end()) {
-                    HttpResponse response = get_routes[route_key]();
-                    string res = 
-                        "HTTP/1.1 " + response.response_code + " " + response.status_text + "\r\n" +
-                        "Content-Type: " + response.headers["Content-Type"] + "\r\n\n" +
-                        response.body;
+                if (routes.find(route_key) != routes.end()) {
+                    HttpResponse response = routes[route_key](arg);
+                    string res = "HTTP/1.1 " + response.response_code + " " + response.status_text + "\r\n";
+                    for (const auto& [key, value] : response.headers) {
+                        res += key + ": " + value + "\r\n";
+                    }
+                    res += "\r\n" + response.body;
 
                     send(client_fd, res.c_str(), res.size(), 0);
                 } else {
@@ -164,12 +205,13 @@ int main() {
                 string item = item_.substr(5,item_.size());
                 vector<string> header = split_string(line); 
                 string route_key = header[0] + " " + header[1];
-                if(post_routes.find(route_key)!=post_routes.end()){
-                    HttpResponse response = post_routes[route_key](regex_replace(item,regex("\\+")," "));
-                    string res = 
-                        "HTTP/1.1 " + response.response_code + " " + response.status_text + "\r\n" +
-                        "Content-Type: " + response.headers["Content-Type"] + "\r\n\n" +
-                        response.body;
+                if(routes.find(route_key)!=routes.end()){
+                    HttpResponse response = routes[route_key](regex_replace(item,regex("\\+")," "));
+                    string res = "HTTP/1.1 " + response.response_code + " " + response.status_text + "\r\n";
+                    for (const auto& [key, value] : response.headers) {
+                        res += key + ": " + value + "\r\n";
+                    }
+                    res += "\r\n" + response.body;
                     send(client_fd, res.c_str(),res.size(),0);
                 } else{
                     string not_found = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\n404 Not Found";
